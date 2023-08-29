ea954e86e6fbdd33c031e7d48d6ecb77026e1c9f
mateuscp <mateuscandicocp@gmail.com>
2023-08-29 09:00:09 -0300

    Tipos Primitivos - JS

---
+++ Aula_02-Variáveis/script.js
@@ -0,0 +1,13 @@
+//Tipos primitivos
+
+//boolean
+var bool = false;
+"Info:", console.log("Tipo:", typeof bool, "| Info:", bool);
+
+//number
+var num = 10;
+console.log("Tipo:", typeof num, "| Info:", num);
+
+//string
+var nome = "Mateus";
+console.log("Tipo:", typeof nome, "| Info:", nome);
