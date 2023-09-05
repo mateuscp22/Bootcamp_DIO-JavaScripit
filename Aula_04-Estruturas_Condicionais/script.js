95e9f621a9f64ebc8480e3a6a5db52f3e6f10629
mateuscp <mateuscandicocp@gmail.com>
2023-09-05 18:48:58 -0300

    If, else, else_if e If ternário

---
+++ Aula_04-Estruturas_Condicionais/script.js
@@ -0,0 +1,25 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+//usando if ternário
+
+jogador1 != -1 && jogador2 != -1
+  ? console.log("Os jogadores são validos")
+  : console.log("Jogadores inválidos");
+
+/* if (jogador1 != -1) { */
+//usadno if
+if (jogador1 > 0 && jogador2 == 0) {
+  console.log("Jogador 1 marcou um ponto.", jogador1);
+  placar = jogador1 > jogador2;
+}
+//usadno else if
+else if (jogador2 > 0 && jogador1 == 0) {
+  console.log("Jogador 2 marcou um ponto.", jogador2);
+  placar = jogador2 > jogador1;
+}
+//usando else
+else {
+  console.log("Ninguém marcou ponto.");
+}
