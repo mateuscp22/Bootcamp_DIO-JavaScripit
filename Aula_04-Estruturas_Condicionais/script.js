6eac5ce6f2d114c4cbddf1e8000e62ca134a1a4b
mateuscp <mateuscandicocp@gmail.com>
2023-09-05 19:15:45 -0300

    Extruturas de repetição

--- Aula_04-Estruturas_Condicionais/script.js
+++ Aula_04-Estruturas_Condicionais/script.js
@@ -8,7 +8,6 @@ jogador1 != -1 && jogador2 != -1
   ? console.log("Os jogadores são validos")
   : console.log("Jogadores inválidos");
 
-/* if (jogador1 != -1) { */
 //usadno if
 if (jogador1 > 0 && jogador2 == 0) {
   console.log("Jogador 1 marcou um ponto.", jogador1);
@@ -35,3 +34,54 @@ switch (placar) {
   default:
     console.log("Ninguém ganhou!");
 }
+
+// EXTRUTURAS DE REPETIÇÃO
+
+let array = ["valor1", "valor2", "valor3", "valor4", "valor5"];
+
+let object = {
+  propriedade1: "valor1",
+  propriedade2: "valor2",
+  propriedade3: "valor3",
+};
+
+// For - Executa uma intrução até que ela seja falsa
+for (let i = 0; i < array.length; i++) {
+  console.log(i);
+}
+
+// For/in - Executa repetição apartir de uma propriedade
+// Com array
+for (i in array) {
+  console.log(i);
+}
+
+// Com object
+for (i in object) {
+  console.log(i);
+}
+
+// For/of - Executa repetição a partir de um valor
+// Com array
+for (i of array) {
+  console.log(i);
+}
+
+// Com object
+for (i of object.propriedade1) {
+  console.log(i);
+}
+
+//While
+var a = 0;
+
+while (a < 10) {
+  a++;
+  console.log(a);
+}
+
+// Do/While
+do {
+  a++;
+  console.log(a);
+} while (a < 12);
