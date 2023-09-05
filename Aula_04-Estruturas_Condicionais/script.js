77796fa590ea042e630d88860d2533ded37fb34b
mateuscp <mateuscandicocp@gmail.com>
2023-09-05 18:54:19 -0300

    switch case

--- Aula_04-Estruturas_Condicionais/script.js
+++ Aula_04-Estruturas_Condicionais/script.js
@@ -23,3 +23,15 @@ else if (jogador2 > 0 && jogador1 == 0) {
 else {
   console.log("Ninguém marcou ponto.");
 }
+
+// switch case
+switch (placar) {
+  case (placar = jogador1 > jogador2):
+    console.log("O jogador 1 ganhou!");
+    break;
+  case (placar = jogador2 > jogador1):
+    console.log("O jogador 2 ganhou!");
+    break;
+  default:
+    console.log("Ninguém ganhou!");
+}
